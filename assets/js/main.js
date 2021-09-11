9400eae41bcdbb4d5ef60251abaa6e7520dee245
leandrobrandao <leandrobrandao@unasus.gov.br>
2021-09-11 14:56:48 -0300

    [FIX] ajustes gerais

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,9 +114,3 @@ $(document).on('click', '.botaoComecar', function (e) {
   e.preventDefault();
   $('html,body').animate({ scrollTop: ($("#topico1").offset().top) }, 500);
 });
-
-//Carregamento do header e do footer em todas as paginas
-$(function () {
-  $("#header").load("../../header.html");
-  $("#footer").load("../../footer.html");
-});
